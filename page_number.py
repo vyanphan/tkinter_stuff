089a949d890c11c6dab038409a4f37bad0a981f6
Vy An Phan <pvivian@qti.qualcomm.com>
2017-06-05 09:51:18 -0700

    an entry box that only takes numbers

--- page_number.py
+++ page_number.py
@@ -6,3 +6,10 @@ def jump(event):
         print(str(page_input))
     except:
         pass
+
+w = Tk()
+Label(w, text="Page Number:").grid(row=0)
+entry = Entry(w)
+entry.grid(row=0, column=1)
+entry.bind("<Return>", jump)
+w.mainloop()
